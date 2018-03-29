d840416e59932168204130871ab0a8cb6615d0f0
Horváth Máté <ifjmate@gmail.com>
2018-03-29 14:39:14 +0200

    Add menu to game

--- src/com/codecool/klondike/Klondike.java
+++ src/com/codecool/klondike/Klondike.java
@@ -2,8 +2,11 @@ package com.codecool.klondike;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.scene.control.MenuBar;
 
 public class Klondike extends Application {
 
@@ -19,9 +22,20 @@ public class Klondike extends Application {
         Card.loadCardImages();
         Game game = new Game();
         game.setTableBackground(new Image("/table/green.png"));
-
+        MenuBar menuBar = new MenuBar();
+        Menu fileMenu = new Menu("File");
+        MenuItem restart = new MenuItem("Restart");
+        MenuItem fullScreen = new MenuItem("FullScreen");
+        restart.setOnAction(e -> {
+            game.restartGame();
+            game.getChildren().add(menuBar);
+        });
+        fullScreen.setOnAction(e -> primaryStage.setFullScreen(true));
+        fileMenu.getItems().addAll(restart, fullScreen);
+        menuBar.getMenus().add(fileMenu);
         primaryStage.setTitle("Klondike Solitaire");
         primaryStage.setScene(new Scene(game, WINDOW_WIDTH, WINDOW_HEIGHT));
+        game.getChildren().add(menuBar);
         primaryStage.show();
     }
 
